8ccc08c3a4f56d6f0e7d17be2b51770e39c6871b
Ethan Hapurne <ethan.hapurne@gmail.com>
2025-05-13 18:45:24 -0400

    Final Changes Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,24 +1,17 @@
 public class Reduce {
     public static void main(String[] args) {
         int i = 100;
-        int count = getCount(i);
-        System.out.println(count);
-    }
-
-    private static int getCount(int i) {
         int count = 0;
-        while (i > 0){
+        while (i > 0) {
             boolean isEven = i % 2 == 0;
 
-            if (isEven){
+            if (isEven) {
                 i = i / 2;
-            }
-
-            else {
+            } else {
                 i--;
             }
             count++;
         }
-        return count;
+        System.out.println(count);
     }
 }
